d0356ebc0f427cb61f39f617434d2b2fc48db189
mahedi1318 <mahedi.mern@gmail.com>
2025-03-11 21:05:35 +0600

    footer section 90% done

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router'
 import Navbar from '../components/header/Navbar'
+import Footer from '../components/footer/Footer'
 
 const RootLayout = () => {
   return (
     <>
     <Navbar />
+    <main className='min-h-screen'>
       <Outlet />
+    </main>
+    <Footer />
     </>
   )
 }
