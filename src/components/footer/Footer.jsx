f7e1676c05b66bcc13eb21738b10d597f90da387
mahedi1318 <mahedi.mern@gmail.com>
2025-03-16 17:17:24 +0600

    navbar and hero section and about section and footer section fully design complite but not responsive complte

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,56 +1,96 @@
 import React from 'react'
+import footerlogo from "../../assets/image/logo-1.png"
+import { Link } from 'react-router'
+import { FaFacebookF } from "react-icons/fa";
+import { IoLogoTwitter } from "react-icons/io5";
+import { FaYoutube } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <>
-      <footer className="footer footer-horizontal footer-center bg-amber-100 text-base-content rounded p-10">
-        <nav className="grid grid-flow-col gap-4">
-            <a className="link link-hover">About us</a>
-            <a className="link link-hover">Contact</a>
-            <a className="link link-hover">Jobs</a>
-            <a className="link link-hover">Press kit</a>
-        </nav>
-        <nav>
-            <div className="grid grid-flow-col gap-4">
-            <a>
-                <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current">
-                <path
-                    d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-                </svg>
-            </a>
-            <a>
-                <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current">
-                <path
-                    d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-                </svg>
-            </a>
-            <a>
-                <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current">
-                <path
-                    d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-                </svg>
-            </a>
+    <footer className='bg-[#000] py-16'>
+        <div className="container">
+            <div className="grid grid-cols-6 gap-[25px]">
+                <div className=" col-span-2">
+                    <Link to="" >
+                        <img className='w-[40%]' src={footerlogo} alt="" />
+                    </Link>
+                    <p className='text-[#ccc] w-[350px] mt-5'>Our secure online donation platform allows you to make contributions quickly and safely. Choose from various.</p>
+                    <button className='btn bg-[#1A685B] border-0 text-white outline-0 inline-block mt-8'>Donate Now</button>
+                </div>
+                <div className="">
+                    <h3 className='mb-5 young_serif text-[22px] text-white font-bold'>Quick Links</h3>
+                    <ul className='text-[#ccc] jost text-[16px]'>
+                        <li className='mb-3'>
+                            <Link to="/" >Home</Link>
+                        </li>
+                        <li className='mb-3'>
+                            <Link to="/" >About</Link>
+                        </li>
+                        <li className='mb-3'>
+                            <Link to="/" >Donation</Link>
+                        </li>
+                        <li className='mb-3'>
+                            <Link to="/" >Contact</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="">
+                    <h3 className='mb-5 young_serif text-[22px] text-white font-bold'>Our Service</h3>
+                    <ul className='text-[#ccc] jost text-[16px]'>
+                        <li className='mb-3'>
+                            <Link to="/" >Give Donation</Link>
+                        </li>
+                        <li className='mb-3'>
+                            <Link to="/" >Education Support</Link>
+                        </li>
+                        <li className='mb-3'>
+                            <Link to="/" >Food Support</Link>
+                        </li>
+                        <li className='mb-3'>
+                            <Link to="/" >Our Campaign</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="bg-[#13443c] border border-[#039b7c] col-span-2 rounded-3xl px-[30px] py-[20px] relative">
+                    <h3 className='mb-5 young_serif text-[22px] text-white font-bold'>Our Service</h3>
+                    <div className="space-y-4">
+                        <div className="">
+                            <span className='block jost text-[18px] font-semibold text-white'>Call us any time:</span>
+                            <Link className='jost text-[15px] font-normal text-[#ccc]' to="">+880 123 456 789</Link>
+                        </div>
+                        <div className="">
+                            <span className='block jost text-[18px] font-semibold text-white'>Email us any time:</span>
+                            <Link className='jost text-[16px] font-normal text-[#ccc]' to="">Example@gmail.com</Link>
+                        </div>
+                    </div>
+                    <div className="p-1 border-1 border-[#039b7c] rounded-full absolute top-1.5 right-1">
+                        <Link to="/" >
+                            <div className="mb-3.5 w-[45px] h-[45px] bg-white rounded-full flex items-center justify-center group hover:bg-[#039b7c] transition-all duration-300">
+                                <FaFacebookF className='text-[20px] group-hover:text-white transition-all duration-300' />
+                            </div>
+                        </Link>
+                        <Link to="/">
+                            <div className="mb-3.5 w-[45px] h-[45px] bg-white rounded-full flex items-center justify-center group hover:bg-[#039b7c] transition-all duration-300">
+                                <IoLogoTwitter className='text-[20px] group-hover:text-white transition-all duration-300' />
+                            </div>
+                        </Link>
+                        <Link to="/">
+                            <div className="mb-3.5 w-[45px] h-[45px] bg-white rounded-full flex items-center justify-center group hover:bg-[#039b7c] transition-all duration-300">
+                                <FaYoutube className='text-[20px] group-hover:text-white transition-all duration-300' />
+                            </div>
+                        </Link>
+                        <Link to="/">
+                            <div className="w-[45px] h-[45px] bg-white rounded-full flex items-center justify-center group hover:bg-[#039b7c] transition-all duration-300">
+                                <FaLinkedinIn className='text-[20px] group-hover:text-white transition-all duration-300' />
+                            </div>
+                        </Link>
+                    </div>
+                </div>
             </div>
-        </nav>
-        <aside>
-            <p>Copyright © {new Date().getFullYear()} - All right reserved by Winter Donation</p>
-        </aside>
-        </footer>
+        </div>
+    </footer>
     </>
   )
 }
--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router'
+import logo from "../../assets/image/loggo.png"
 
 const Navbar = () => {
 
@@ -19,30 +20,34 @@ let links = (
 
   return (
     <>
-      <div className="navbar container border-b border-gray-400">
-        <div className="navbar-start">
-            <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+    <div className="bg-[#f7f7f7]">
+      <div className="navbar container">        
+            <div className="navbar-start">
+                <div className="dropdown">
+                <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                </div>
+                <ul
+                    tabIndex={0}
+                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow jost text-[16px]">
+                    {links}
+                </ul>
+                </div>
+                <Link to="/" className=" text-xl">
+                    <img className='w-[25%]' src={logo} alt="" />
+                </Link>
             </div>
-            <ul
-                tabIndex={0}
-                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                {links}
-            </ul>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 jost text-[16px]">
+                    {links}
+                </ul>
             </div>
-            <Link to="/" className=" text-xl">winter Donation</Link>
-        </div>
-        <div className="navbar-center hidden lg:flex">
-            <ul className="menu menu-horizontal px-1">
-                {links}
-            </ul>
-        </div>
-        <div className="navbar-end space-x-3">
-            <Link className='btn btn-info' to="/login">Login</Link>
-            <Link className='btn btn-info' to="/register">Register</Link>
-          
-        </div>
+            <div className="navbar-end space-x-3">
+                <Link className='btn bg-[#1A685B] text-white jost' to="/login">Login</Link>
+                <Link className='btn bg-[#1A685B] text-white jost' to="/register">Register</Link>
+            
+            </div>
+            </div>
         </div>
     </>
   )
